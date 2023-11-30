0853201d2265b8a79eff0a17a40bd5606441127a
Gladson Robinson <gladson.robinson@deliveryhero.com>
2023-11-30 14:28:14 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-900 to-purple-900 text-white">
-      <div className="text-4xl md:text-6xl lg:text-8xl">Hello Im a human, and i belive in wrting test cases</div>
+      <div className="text-4xl md:text-6xl lg:text-8xl">Hello Im a human, <br/> and i belive in wrting test cases</div>
     </main>
   );
 }
